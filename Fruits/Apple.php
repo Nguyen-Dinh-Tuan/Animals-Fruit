f124bec17a248c56d33d6d4e6b985a1ea5af0117
tuan <tuandinh982002@gmail.com>
2020-11-12 11:39:57 +0700

    thuchanh

---
+++ Fruits/Apple.php
@@ -0,0 +1,11 @@
+<?php
+include_once (dirname(__FILE__) .'/../Abstrackclass/Fruits.php');
+
+class Apple extends Fruit
+{
+    public function howToEat()
+    {
+        return "Apple could be slided";
+        // TODO: Implement howToEat() method.
+    }
+}
